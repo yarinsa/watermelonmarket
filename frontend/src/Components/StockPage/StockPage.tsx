7a8709a52694ed22c614d5d40496ce77a6732013
yarinsa <yarinsasson2@gmail.com>
2020-04-30 00:25:13 +0300

    fix

--- frontend/src/Components/StockPage/StockPage.tsx
+++ frontend/src/Components/StockPage/StockPage.tsx
@@ -7,9 +7,16 @@ import { useRouteMatch } from "react-router-dom";
 import { MatchParams } from "../../App";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
-import { ReactComponent as Loader } from '../../assets/Loader.svg'
-import { LineChart, Line, Legend, Tooltip, ResponsiveContainer, ComposedChart } from "recharts";
-import {formatDataToChart} from '../../utils.service'
+import { ReactComponent as Loader } from "../../assets/Loader.svg";
+import {
+  LineChart,
+  Line,
+  Legend,
+  Tooltip,
+  ResponsiveContainer,
+  ComposedChart,
+} from "recharts";
+import { formatDataToChart } from "../../utils.service";
 import { Chart } from "../Chart";
 
 interface StockPageProps {}
@@ -23,47 +30,53 @@ export const StockPage: React.FC<StockPageProps> = ({}) => {
     },
   });
 
-  if(data) console.log(data.stock.quote.change)
   // console.log(error)
-  
+
   return (
     <Root>
-      {!loading && data && 
-      (<div>
-      <Header>
-        <TitleRow>{data.stock.quote.price.toFixed(2)}</TitleRow>
-        <SecondaryRow>
-          <SecondaryText isPositive={data.stock.quote.change > 0} >{data.stock.quote.change.toFixed(2)} ({data.stock.quote.changePercent.toFixed(2)}%)</SecondaryText>
-           <Symbol>{data.stock.symbol}</Symbol>
-        </SecondaryRow>
-        <SecondaryRow>
-          <SecondaryText>{data.stock.market.exchangeName} {data.stock.market.marketState}</SecondaryText>
-          <SecondaryText>{data.stock.name}</SecondaryText>
-        </SecondaryRow>
-      </Header>
-      <ChartContainer> 
-        <Chart chartData={data.stock.chartData} symbol={data.stock.symbol} />
-
-      </ChartContainer>
-      <InfoContainer>
-        <InfoItem>
-          <InfoItemTitle>Stats</InfoItemTitle>
-          <KeyStatisticsTable {...data.stock} />
-        </InfoItem>
-        <InfoItem>
-          <InfoItemTitle>About</InfoItemTitle>
-          <CompanyDescription>
-            {data.stock.companyProfile.description}
-          </CompanyDescription>
-          <ShowMore>Show more</ShowMore>
-        </InfoItem>
-      </InfoContainer>
-      </div>)}
-      {loading && <Loader/>}
+      {!loading && data && (
+        <div>
+          <Header>
+            <TitleRow>{data.stock.quote.price.toFixed(2)}</TitleRow>
+            <SecondaryRow>
+              <SecondaryText isPositive={data.stock.quote.change > 0}>
+                {data.stock.quote.change.toFixed(2)} (
+                {data.stock.quote.changePercent.toFixed(2)}%)
+              </SecondaryText>
+              <Symbol>{data.stock.symbol}</Symbol>
+            </SecondaryRow>
+            <SecondaryRow>
+              <SecondaryText>
+                {data.stock.market.exchangeName} {data.stock.market.marketState}
+              </SecondaryText>
+              <SecondaryText>{data.stock.name}</SecondaryText>
+            </SecondaryRow>
+          </Header>
+          <ChartContainer>
+            <Chart
+              chartData={data.stock.chartData}
+              symbol={data.stock.symbol}
+            />
+          </ChartContainer>
+          <InfoContainer>
+            <InfoItem>
+              <InfoItemTitle>Stats</InfoItemTitle>
+              <KeyStatisticsTable {...data.stock} />
+            </InfoItem>
+            <InfoItem>
+              <InfoItemTitle>About</InfoItemTitle>
+              <CompanyDescription>
+                {data.stock.companyProfile.description}
+              </CompanyDescription>
+              <ShowMore>Show more</ShowMore>
+            </InfoItem>
+          </InfoContainer>
+        </div>
+      )}
+      {loading && <Loader />}
     </Root>
   );
 };
-
 
 const Root = styled.section`
   padding: 0 20px;
@@ -84,16 +97,16 @@ const SecondaryRow = styled.div`
   justify-content: space-between;
 `;
 
-const SecondaryText = styled.span<{isPositive? : boolean}>`
+const SecondaryText = styled.span<{ isPositive?: boolean }>`
   display: block;
   color: ${(props) => {
-    if (props.isPositive=== false){
-      return "#FF4D2D"
+    if (props.isPositive === false) {
+      return "#FF4D2D";
     }
-    if (props.isPositive=== true){
-      return "#21ce99"
+    if (props.isPositive === true) {
+      return "#21ce99";
     }
-    return "#999"
+    return "#999";
   }};
   font-size: 14px;
   line-height: 18px;
@@ -107,7 +120,7 @@ const Symbol = styled.span`
 const ChartContainer = styled.div`
   flex: 1;
   height: 300px;
-  width:100%;
+  width: 100%;
   border-bottom: 1px solid rgba(41, 41, 41, 0.15);
 `;
 
@@ -148,9 +161,10 @@ const ShowMore = styled.a`
 `;
 
 const StyledChart = styled(ComposedChart)`
-.recharts-layer.recharts-line-dots{
-  display:none;
-}`
+  .recharts-layer.recharts-line-dots {
+    display: none;
+  }
+`;
 
 const GET_STOCK_INFO = gql`
   query StockInfo($symbol: ID!) {
@@ -170,7 +184,7 @@ const GET_STOCK_INFO = gql`
       companyProfile {
         description
       }
-      priceHistory{
+      priceHistory {
         fiftyTwoWeekLow
         fiftyTwoWeekHigh
       }
@@ -179,10 +193,10 @@ const GET_STOCK_INFO = gql`
         exchangeName
         currency
       }
-      chartData(timeRange:  YEAR,interval: DAY){
+      chartData(timeRange: YEAR, interval: DAY) {
         time
         price
       }
-   }
+    }
   }
 `;
